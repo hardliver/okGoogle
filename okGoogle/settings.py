bb52e92a31f0e7b846ee0649ae5d0526e836cdb9
hammerandnail <yelionel26@gmail.com>
2019-01-28 13:14:46 +0800

    Add exceptions.py

--- okGoogle/settings.py
+++ okGoogle/settings.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from django.conf import settings
+from .exceptions import ActionOnGoogleSettingError
 
 
 USER_SETTINGS = getattr(settings, 'ACTIONS_ON_GOOGLE', None)
@@ -18,7 +19,7 @@ def mergeSetting(USER_SETTINGS, DEFAULT_SETTINGS):
                 if type(v)==type(defaultValue):
                     _settings[k] = v
                 else:
-                    raise Exception('ACTIONS_ON_GOOGLE\'s Key {} that this value type should be {}'.format(k, type(defaultValue).__name__))
+                    raise ActionOnGoogleSettingError('{} value type should be {}'.format(k, type(defaultValue).__name__))
     return _settings
 
 ACTIONS_ON_GOOGLE = mergeSetting(USER_SETTINGS, DEFAULT_SETTINGS)
